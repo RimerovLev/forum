02e6499d3147d4b9adb0f6f518be04b004ff7ce9
levrimerov <leo.rimerov@gmail.com>
2024-12-05 13:26:08 +0200

    added session

--- src/main/java/telran/java51/webserviceforum/security/AuthorizationConfig.java
+++ src/main/java/telran/java51/webserviceforum/security/AuthorizationConfig.java
@@ -1,16 +1,13 @@
 package telran.java51.webserviceforum.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import telran.java51.webserviceforum.accounting.model.Role;
@@ -27,6 +24,7 @@ public class AuthorizationConfig {
     public SecurityFilterChain web(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/account/register", "/forum/posts/**")
                         .permitAll()
